a6a52d69bde6c7a03a3862aa35e7140333687106
John Hoang <johnhoang1994@gmail.com>
2022-04-15 00:17:50 -0700

    fix: stack is individual strings

--- src/portfolio.js
+++ src/portfolio.js
@@ -43,7 +43,7 @@ const projects = [
     name: 'Web3-blog',
     description:
       'A fullstack web3 personal blog. Some features are dark mode, filtering by tags, pagination, and sorting.',
-    stack: ['React, Ethersjs, Solidity, Next, Mui, Chai, Graphql, The Graph, IPFS, Hardhat'],
+    stack: ['React', 'Ethersjs', 'Solidity', 'Next', 'Mui', 'Chai', 'Graphql', 'The Graph', 'IPFS', 'Hardhat'],
     sourceCode: 'https://github.com/jhoan2/web3-blog',
     livePreview: 'https://web3-blog-beta.vercel.app/',
   },
